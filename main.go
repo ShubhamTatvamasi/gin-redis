e9d5349e51bc14ea5ac6fd7a9795844504803456
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2020-06-09 19:05:34 +0530

    modified:   main.go

--- main.go
+++ main.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-	"context"
-	"os"
-	"strconv"
-	"github.com/gin-gonic/gin"
-	"github.com/go-redis/redis/v8"
+  "context"
+  "os"
+  "strconv"
+  "github.com/gin-gonic/gin"
+  "github.com/go-redis/redis/v8"
 )
 
 func main() {
 
-	const greetings = "Welcome to Gin "
-	var ctx = context.Background()
-	redisHost := os.Getenv("REDIS_HOST")
-	redisPort := os.Getenv("REDIS_PORT")
-	redisPath := redisHost + ":" + redisPort
+  const greetings = "Welcome to Gin "
+  var ctx = context.Background()
+  redisHost := os.Getenv("REDIS_HOST")
+  redisPort := os.Getenv("REDIS_PORT")
+  redisPath := redisHost + ":" + redisPort
 
-	client := redis.NewClient(&redis.Options{
-		Addr: redisPath,
-	})
+  client := redis.NewClient(&redis.Options{
+    Addr: redisPath,
+  })
 
-	r := gin.Default()
+  r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
+  r.GET("/", func(c *gin.Context) {
 
-		number, err := client.Incr(ctx, "number").Result()
-		if err != nil {
-			panic(err)
-		}
+    number, err := client.Incr(ctx, "number").Result()
+    if err != nil {
+      panic(err)
+    }
 
-		c.String(200, greetings + strconv.FormatInt(number, 10) + "\n")
+    c.String(200, greetings + strconv.FormatInt(number, 10) + "\n")
 
-	})
+  })
 
-	r.Run(":80")
+  r.Run(":80")
 
 }
